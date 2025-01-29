aeeb04e103feef4d688d0471e57a30d5df45a075
wfox5510 <wfox5510@gmail.com>
2025-01-29 13:05:43 +0800

    Adjust Bootstrap customization

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'; // 引入 CSS
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./style/main.scss";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(<App />);
