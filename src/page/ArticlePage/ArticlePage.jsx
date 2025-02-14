38515348361f29f3fe28e6d5606cbf4b1225f0f7
wfox5510 <wfox5510@gmail.com>
2025-02-14 16:49:29 +0800

    Add related-articles section and import Swiper

--- src/page/ArticlePage/ArticlePage.jsx
+++ src/page/ArticlePage/ArticlePage.jsx
@@ -1,6 +1,10 @@
 import Navbar from "../../component/Navbar/Navbar";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import ArticleCard from "../../component/ArticleCard/ArticleCard";
 import CommentBox from "../../component/CommentBox/CommentBox";
 import CommentReply from "../../component/CommentReply/CommentReply";
+import Footer from "../../component/Footer/Footer";
 const ArticlePage = () => {
   return (
     <>
@@ -34,7 +38,70 @@ const ArticlePage = () => {
           </form>
         </div>
       </section>
-
+      <section>
+        <div className="container py-15">
+          <h3 className="fs-3 text-primary fw-bold mb-5">相關文章</h3>
+          <nav class="related-articles nav mb-5 gap-5">
+            <a class="nav-link p-0 active" aria-current="page" href="#">
+              數位時代
+            </a>
+            <a class="nav-link p-0" href="#">
+              AI生成
+            </a>
+            <a class="nav-link p-0" href="#">
+              未來創作
+            </a>
+          </nav>
+          <div className="d-none d-md-flex row row-cols-2 row-cols-xl-4 g-lg-6 g-3 mb-10">
+            <div className="col">
+              <ArticleCard />
+            </div>
+            <div className="col">
+              <ArticleCard />
+            </div>
+            <div className="col">
+              <ArticleCard />
+            </div>
+            <div className="col">
+              <ArticleCard />
+            </div>
+          </div>
+          <div className="d-block d-md-none">
+            <Swiper
+              style={{
+                "--swiper-pagination-color": "#E77605",
+                "--swiper-pagination-bullet-inactive-color": "#EAEAEA",
+                "--swiper-pagination-bullet-inactive-opacity": "1",
+                margin: "-12px",
+                padding: "12px",
+              }}
+              className="mb-6 pb-11"
+              modules={[Pagination, Navigation]}
+              pagination={{
+                clickable: true,
+                bulletClass:
+                  "swiper-pagination-bullet swiper-pagination-bullet-mx-6",
+              }}
+              loop={true}
+              spaceBetween={"24px"}
+            >
+              <SwiperSlide>
+                <ArticleCard />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ArticleCard />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ArticleCard />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ArticleCard />
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </>
   );
 };
