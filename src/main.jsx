3bd9c8fb19ce35f77f6ab66bbddd49bbeab7a3ad
wfox5510 <wfox5510@gmail.com>
2025-02-16 15:43:32 +0800

    Use React Router for routing instead of App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./style/main.scss";
-import App from "./App.jsx";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+import router from "./router/router";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={createHashRouter(router)} />
+);
