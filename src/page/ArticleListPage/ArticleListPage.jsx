9bfcc0c89267f8f1c7b2ae499e655276afead87b
wfox5510 <wfox5510@gmail.com>
2025-02-18 16:25:17 +0800

    Complete ArticleListPage layout

--- src/page/ArticleListPage/ArticleListPage.jsx
+++ src/page/ArticleListPage/ArticleListPage.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../../component/Navbar/Navbar";
-
+import Footer from "../../component/Footer/Footer"
 const ArticleListPage = () => {
   return (
     <>
@@ -70,7 +70,7 @@ const ArticleListPage = () => {
                     <li>
                       <a href="#" className="card border-0 p-5">
                         <img
-                          src="https://github.com/wfox5510/wordSapve-imgRepo/blob/main/articleList-hot1.png?raw=true"
+                          src="https://github.com/wfox5510/wordSapce-imgRepo/blob/main/articleList-hot1.png?raw=true"
                           className="card-img-top mb-2"
                           alt="..."
                         />
@@ -171,18 +171,69 @@ const ArticleListPage = () => {
           className="container py-10 px-5 border"
           style={{ backgroundColor: "#FFFDFB" }}
         >
-          <h2 className="text-primary fw-bold">文章列表</h2>
-          <div className="d-flex justify-content-between">
+          <h2 className="text-primary fw-bold mb-5">文章列表</h2>
+          <div className="d-flex justify-content-between mb-5">
             <div className="article-list-select-wrap">
-              <select className="text-dark p-3 border" name="article-list-class" id="article-list-class">
+              <select
+                className="text-dark p-3 border"
+                name="article-list-class"
+                id="article-list-class"
+              >
                 <option value="allArticle">全部內容</option>
                 <option value="opinionsSharing">心得分享</option>
                 <option value="bookReport">書籍評論</option>
               </select>
             </div>
+            <button className="btn btn-lg btn-primary fw-bold lh-sm">
+              新增文章
+            </button>
           </div>
+          <ul className="list-unstyled d-flex flex-column gap-5">
+            {Array.from({ length: 10 }).map(() => {
+              return (
+                <li className="rounded-2 border">
+                  <a className="d-flex justify-content-between p-5" href="#">
+                    <div className="d-flex flex-column gap-5 me-6">
+                      <h3 className="text-primary fs-7 fw-bold text-truncate-2lines lh-sm">
+                        提升專注力的五個簡單方法，讓你事半功倍
+                      </h3>
+                      <p className="text-truncate-2lines ">
+                        專注力是現代生活中不可或缺的能力，但往往被繁忙的生活打散。這篇文章分享五個實用又簡單的技巧，幫助你在工作和生活中找回專注，實現更高效率。
+                      </p>
+                      <div className="d-flex align-items-center gap-3 mt-auto">
+                        <span className="text-gray">2024/12/22</span>
+                        <span className="text-gray d-flex align-items-center gap-1">
+                          143
+                          <span className="material-icons-outlined">
+                            favorite
+                          </span>
+                        </span>
+                        <span className=" text-gray d-flex align-items-center gap-1">
+                          16
+                          <span className="material-icons-outlined">
+                            chat_bubble
+                          </span>
+                        </span>
+                        <span class="material-symbols-outlined text-primary icon-fill">
+                          keep
+                        </span>
+                      </div>
+                    </div>
+                    <img
+                      className="object-fit-cover"
+                      src="https://github.com/wfox5510/wordSapce-imgRepo/blob/main/articleList-article1.png?raw=true"
+                      alt=""
+                      height={"100%"}
+                      width={"266px"}
+                    />
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </section>
+      <Footer/>
     </>
   );
 };
