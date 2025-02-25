4b7bad38a19dbbdbdde33509957f6ff271f4c38d
lifaa12 <andy0312huang@gmail.com>
2025-02-25 17:18:11 +0800

    BlogHome_mobileHeader

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,13 +1,19 @@
 import HomePage from "../page/HomePage/HomePage";
 import ArticlePage from "../page/ArticlePage/ArticlePage";
+import BlogHome from "../page/BlogPage/BlogHome";
 
 const router = [
   {
     path: "/",
     element: <HomePage />,
   },
-  { path: "/article", 
-    element: <ArticlePage /> 
+  {
+    path: "/article",
+    element: <ArticlePage />
+  },
+  {
+    path: "/blog",
+    element: <BlogHome />
   },
 ];
 
