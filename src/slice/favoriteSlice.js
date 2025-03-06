0dc5fe844daebea69d503cfa0c188154ea7741d0
wfox5510 <wfox5510@gmail.com>
2025-03-06 23:16:02 +0800

    Adjust favoriteArticle state default value to prevent TypeError

--- src/slice/favoriteSlice.js
+++ src/slice/favoriteSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit"
 const favoriteSlice = createSlice({
   name:"favorite",
   initialState:{
-    favoriteArticle:{}
+    favoriteArticle:[]
   },
   reducers:{
     setFavoriteArticle(state,action){
