91c9abc8c55334ac96981b05935cb02e8c7486d8
lifaa12 <andy0312huang@gmail.com>
2025-03-12 02:28:34 +0800

    fix bugs,變更使用者資訊時，寫入redux

--- src/page/AdminPage/AdminLayout/AdminLayout.jsx
+++ src/page/AdminPage/AdminLayout/AdminLayout.jsx
@@ -1,32 +1,8 @@
 import { Outlet, NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import avatar from "../../../assets/images/avatar-1.png";
-import { useEffect, useState } from "react";
-import axios from "axios";
-const { VITE_API_BASE_URL } = import.meta.env;
-
 
 const AdminLayout = () => {
-  const { isAuthorized, id, username, token } = useSelector(state => state.auth);
-  const [userInfo, setUserInfo] = useState({ username: "", profile_picture: "" })
-  const fetchUserInfo = async () => {
-    if (!token) return;
-    try {
-      const res = await axios.get(`${VITE_API_BASE_URL}/users/${id}`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      setUserInfo({
-        username: res.data.username,
-        profile_picture: res.data.profile_picture || avatar,
-      });
-    } catch (error) {
-      console.error("無法取得使用者資訊", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchUserInfo();
-  }, []);
+  const { isAuthorized, id, username, token, userAvatar } = useSelector(state => state.auth);
   const adminLinkActive = ({ isActive }) => (isActive ? "adminLinkActive" : "");
   return (
     <>
@@ -36,8 +12,8 @@ const AdminLayout = () => {
             <div className="col-md-3 d-none d-md-block">
               <aside className="admin-pc_nav admin_wrap py-10 px-5 rounded-3 border border-gray_light">
                 <div className="d-flex align-items-center flex-column border-bottom border-gray_light mb-5">
-                  <img className="admin-avatar mb-2 rounded-circle" src={userInfo.profile_picture} alt="avatar" />
-                  <p className="mb-2">{userInfo.username}</p>
+                  <img className="admin-avatar mb-2 rounded-circle" src={userAvatar || "https://raw.githubusercontent.com/wfox5510/wordSapce-imgRepo/695229fa8c60c474d3d9dc0d60b25f9539ac74d9/default-avatar.svg"} alt="avatar" />
+                  <p className="mb-2">{username}</p>
                   <p className="text-primary mb-5">編輯個人主頁</p>
                 </div>
                 <nav className="list-unstyled d-flex flex-column admin-layout_nav mb-6 gap-2 align-items-center">
@@ -52,8 +28,8 @@ const AdminLayout = () => {
               <div className="admin_wrap pt-10 pb-5 px-5 rounded-3 border border-gray_light">
                 <div className="admin-mobile-header d-md-none">
                   <div className="d-flex align-items-center flex-column border-bottom border-gray_light mb-5">
-                    <img className="admin-avatar mb-2" src={userInfo.profile_picture} alt="avatar" />
-                    <p className="mb-2">super123</p>
+                    <img className="admin-avatar mb-2" src={userAvatar || "https://raw.githubusercontent.com/wfox5510/wordSapce-imgRepo/695229fa8c60c474d3d9dc0d60b25f9539ac74d9/default-avatar.svg"} alt="avatar" />
+                    <p className="mb-2">{username}</p>
                     <p className="text-primary mb-5">編輯個人主頁</p>
                   </div>
                   <nav className="list-unstyled d-flex justify-content-between admin-layout_nav mb-6">
@@ -63,7 +39,7 @@ const AdminLayout = () => {
                     <NavLink to="background" className={({ isActive }) => `pb-1 link-gray ${isActive ? "adminLinkActive" : ""}`}>管理後臺</NavLink>
                   </nav>
                 </div>
-                <Outlet context={{ fetchUserInfo }} />
+                <Outlet />
               </div>
             </div>
           </div>
